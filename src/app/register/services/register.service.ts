13bd79d1cb76579e3f768dbb8ebcec1b9ee4ac91
rodrigosnts <rodrigo03santos@gmail.com>
2025-11-20 16:59:56 +0000

    Add user registration feature and UI improvements
    Introduces a user registration component with form validation and service integration. Updates authentication and registration routes to include user registration. Refactors home-login and user-login pages for improved UI/UX and navigation, and enhances the main page layout. Adds BitsOfMe logo assets for branding.

--- src/app/register/services/register.service.ts
+++ src/app/register/services/register.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from "@angular/common/http";
 import { Observable } from 'rxjs';
 import { ECRegistrationData } from '../components/ec-register/ec-register';
+import { UserRegistrationData } from '../components/user-register/user-register';
 
 @Injectable({
   providedIn: 'root'
@@ -14,4 +15,9 @@ export class RegisterService {
 registerEC(data: ECRegistrationData): Observable<HttpResponse<void>> {
     return this.http.post<void>(`${this.API}/register/ec-register`, data, {observe: "response"});
   }
+
+registerUser(data: UserRegistrationData): Observable<HttpResponse<void>> {
+    return this.http.post<void>(`${this.API}/register/user-register`, data, { observe: "response" });
+  }
+
 }
