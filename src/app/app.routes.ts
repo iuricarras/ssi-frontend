a4fea110dc672dd1f94f57ceade5d448a002af1e
Eduardo Marciano <89046882+EduardoMarciano@users.noreply.github.com>
2025-12-03 00:40:30 +0000

    fix: visual bug

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeLogin } from './auth/pages/home-login/home-login';
 
 export const APP_ROUTES: Routes = [
-  { path: '', component: HomeLogin },
+  { path: '', redirectTo: 'home/main-page', pathMatch: 'full' },
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.routes').then(m => m.AUTH_ROUTES),
